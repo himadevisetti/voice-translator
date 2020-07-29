a543bccc0eff3a2608ca6788d8fb3069227524f0
Hima Bindu Devisetti <himabindudevi@yahoo.com>
2020-07-29 09:35:51 -0700

    Changed google sign-in button to an icon

--- static/utils.js
+++ static/utils.js
@@ -94,3 +94,33 @@ function loginError(error) {
     // window.location.assign("/login");
 
 }
+
+function googleSignIn() {
+
+    var googleUser = {};
+    // var googleSignIn = function () {
+        gapi.load('auth2', function () {
+            // Retrieve the singleton for the GoogleAuth library and set up the client.
+            auth2 = gapi.auth2.init({
+                client_id: '753356494760-74fh1dbi0tpkkifv1vee1v7u00jbf341.apps.googleusercontent.com',
+                cookiepolicy: 'single_host_origin',
+                // Request scopes in addition to 'profile' and 'email'
+                //scope: 'additional_scope'
+            });
+            attachSignin(document.getElementById('customGoogleBtn'));
+        });
+    // };
+
+}
+
+function attachSignin(element) {
+    console.log(element.id);
+    auth2.attachClickHandler(element, {},
+        function (googleUser) {
+            document.getElementById('name').innerText = "Signed in: " +
+                googleUser.getBasicProfile().getName();
+            onSignIn(googleUser);
+        }, function (error) {
+            alert(JSON.stringify(error, undefined, 2));
+        });
+}
